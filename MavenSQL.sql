4bf66c75eb5a7a660d7504ace8e0905803d7dbfb
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-01-16 15:17:29 +0400

    MavenSQL

---
+++ MavenSQL.sql
@@ -0,0 +1,7 @@
+use DataAPI;
+
+CREATE TABLE University( id int NOT NULL AUTO_INCREMENT ,web_pages  VARCHAR(40),state_province VARCHAR(15),alpha_two_code VARCHAR(15), name VARCHAR(50) ,country VARCHAR(25) ,domains VARCHAR(25) 
+
+,PRIMARY KEY(id));
+
+Select *from university;
